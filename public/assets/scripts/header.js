c98a7ad9fa87448164044a8c60d955ea1d2d03e2
Erik Sannes <erik.sannes@volue.com>
2026-05-20 09:07:35 +0200

    Added rendering of desktop cart nav link, excluding it from the previous "NAV_LINKS", as the HTML structure for it is different.

--- public/assets/scripts/header.js
+++ public/assets/scripts/header.js
@@ -23,31 +23,46 @@ class Header {
         const desktopNavLinks = document.createElement('ul');
         desktopNavLinks.className = 'desktop-nav-links';
         
-        const NAV_LINKS = [
-            { href: 'index.html', label: 'Home', page: 'index' },
-            { href: 'movies.html', label: 'Movies', page: 'movies' },
-            { href: 'contact.html', label: 'Contact', page: 'contact' },
-            { href: 'about.html', label: 'About', page: 'about' },
-            { href: 'cart.html', label: '', page: 'cart' }
+        const TEXT_NAV_LINKS = [
+            { href: 'index.html', title: 'Home', page: 'index' },
+            { href: 'movies.html', title: 'Movies', page: 'movies' },
+            { href: 'contact.html', title: 'Contact', page: 'contact' },
+            { href: 'about.html', title: 'About', page: 'about' }
         ];
         
-        for (let i = 0; i < NAV_LINKS.length; i++)
-            desktopNavLinks.append(this.createNavLink(NAV_LINKS[i]));
-        
+        for (let i = 0; i < TEXT_NAV_LINKS.length; i++)
+            desktopNavLinks.append(this.createTextNavLink(TEXT_NAV_LINKS[i]));
+
+        const cartICon = this.createDesktopCartNavLink();
+        desktopNavLinks.appendChild(cartICon);
+
         desktopNav.append(desktopNavLinks);
         siteHeader.append(logoHomeLink, desktopNav);
         document.querySelector('header')?.append(siteHeader);
     }
 
-    static createNavLink({ href, label, page }) {
+    static createTextNavLink({ href, title, page }) {
         const isActive = document.body.dataset.page === page;
         const li = document.createElement('li');
         const link = document.createElement('a');
         link.href = href;
-        link.textContent = label;
+        link.textContent = title;
         link.className = 'nav-link';
-        if (isActive) link.classList.add('desktop-nav-link-active');
+        if (isActive)
+            link.classList.add('nav-link-active');
         li.append(link);
+        return li;
+    }
+
+    static createDesktopCartNavLink() {
+        const li = document.createElement('li');
+        const link = document.createElement('a');
+        link.href = 'checkout.html';
+        link.ariaLabel = 'Cart';
+        const cartIcon = document.createElement('span');
+        cartIcon.className = 'material-icons';
+        cartIcon.textContent = 'shopping_cart';
+        li.append(link, cartIcon);
         return li;
     }
 }
