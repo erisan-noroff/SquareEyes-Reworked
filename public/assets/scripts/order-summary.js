375e5a79b4fa6410242cf5eed418d7a3191d6b48
erisan-noroff <192410141+erisan-noroff@users.noreply.github.com>
2026-05-24 17:27:47 +0200

    Removed old checkout.js and replaced with new, updated order-summary.js.

---
+++ public/assets/scripts/order-summary.js
@@ -0,0 +1,45 @@
+import { cartInstance } from './cart.js';
+
+export class OrderSummary {
+    init() {
+        if (cartInstance.cartIsEmpty())
+            return;
+        
+        document.querySelector('.checkout-form')?.addEventListener('submit', (e) => {
+            e.preventDefault();
+            cartInstance.emptyCart();
+            document.location = new URL('checkout-success.html', window.location.href).toString();
+        });
+        
+        let totalPrice = 0;
+
+        const cartItemsRow = document.querySelector('.order-summary-item-rows');
+        for (const cartItem of Object.values(cartInstance.cartItems)) {
+            totalPrice += cartItem.moviePrice;
+            
+            const row = document.createElement('div');
+            row.className = 'order-summary-row';
+            
+            const title = document.createElement('p');
+            title.textContent = cartItem.movieTitle;
+            
+            const price = document.createElement('p');
+            price.textContent = `${cartItem.moviePrice},-`;
+            
+            row.append(title, price);
+            cartItemsRow.append(row);
+        }
+        
+        const totalPriceRow = document.createElement('div');
+        totalPriceRow.classList.add('order-summary-row', 'order-summary-total-row');
+        const total = document.createElement('p');
+        total.textContent = 'Total';
+        const totalAmount = document.createElement('p');
+        totalAmount.textContent = `${totalPrice},-`;
+        
+        totalPriceRow.append(total, totalAmount);
+        document.querySelector('.order-summary')?.append(totalPriceRow);
+    }
+}
+
+new OrderSummary().init();
